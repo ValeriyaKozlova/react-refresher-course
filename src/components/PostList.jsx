19d76b1aace00dde91cf48aef57dceb7376e4ecd
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-10-18 17:27:14 +0200

    react transition groupe library

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export default function PostList({ posts, title, remove }) {
   if (!posts.length) {
@@ -12,9 +13,18 @@ export default function PostList({ posts, title, remove }) {
         style={{ textAlign: 'center' }}>
         {title}
       </h1>
-      {posts.map((post, index) => {
-        return <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-      })}
+
+      <TransitionGroup>
+        {posts.map((post, index) =>
+          <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post"
+          >
+            <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   )
 }
